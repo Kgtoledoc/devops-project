60f530352db9708812b8c0c5236c94c7dc265d9e
kgtoledoc <kgtoledoc19@gmail.com>
2022-06-01 11:45:28 -0500

    Changes in setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup (
+  name='calculator',
+  version='2.0.0',
+  packages=find_packages(),
+  include_package_data=True,
+  zip_safe=False,
+  install_requires=[
+    'flask',
+  ]
+)
